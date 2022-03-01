916e57ac698e7787467bbacd39e089847cd6e2be
annkim10 <90731268+annkim10@users.noreply.github.com>
2022-03-01 13:42:37 -0500

    Redo splash

--- src/components/splash/splash.jsx
+++ src/components/splash/splash.jsx
@@ -1,6 +1,7 @@
 import React from "react"
 import { useEffect } from "react"
 import "./splash.css"
+import headshot from "../../images/headshot.png"
 import Timeline from "./timeline"
 
 
@@ -11,16 +12,16 @@ const Splash = () => {
     return (
         <div className="splash-outer-div">
             <div className="splash-inner-div">
-                <div className="splash-left-div">
-                    <div className="splash-header-div">
-                        <h2>Hi, I'm</h2>
-                        <h1 className="splash-name">Ann Kim</h1>
-                        <p className="splash-descrip">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                    </div>                    
-                </div>
-                <div className="splash-right-div">
-                    <Timeline />
-                </div>
+                <div className="copytag">© 2022 by Ann Kim created with React</div>
+               <div className="splash-left-div ">
+                    <img className="headshot-img" src={headshot} />
+                    <h1 className="name-header">It's nice to meet you, <br/> I'm <span>Ann Kim</span></h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore</p>
+               </div>
+               <div className="splash-right-div">
+
+               </div>
+
             </div>
         </div>
     )
