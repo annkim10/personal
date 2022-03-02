def32aa366e5b6f7eec496f4fc3c2b1eb6e4ac60
annkim10 <90731268+annkim10@users.noreply.github.com>
2022-03-01 23:17:51 -0500

    Updated splash

--- src/components/splash/splash.jsx
+++ src/components/splash/splash.jsx
@@ -1,44 +1,42 @@
-import React from "react"
-import { useEffect, useState } from "react"
-import { Link } from "react-router-dom"
+import React, { useEffect } from "react"
+import { useSpring, animated as a, config } from "react-spring";
 import "./splash.css"
-import headshot from "../../images/headshot.png"
 import Timeline from "./timeline"
 
 const Splash = () => {
 
     // useEffect(() => window.scroll(0,0))
 
-    const [mobile, setMobile] = useState(false)
+    const [load, setLoad] = React.useState(false)
 
-    useEffect(() => {
-        showMobileMenu();
-        window.addEventListener('resize', showMobileMenu);
-        // return window.removeEventListener('resize', showMobileMenu)     
-    }, [])
+    useEffect(() => setLoad(true))
 
-    const showMobileMenu = () => {
-        if (window.innerWidth <= 960) {
-            setMobile(true)
-        } else {
-            setMobile(false)
-        }
-    }
-    console.log(mobile)
+    const animateDown = useSpring({
+        opacity: load ? 1 : 0,
+        marginTop: load ? 0 : -500, 
+        config: config.slow
+    })
+
+    console.log("load", load)
+
+
     return (
         <div className="splash-outer-div">
             <div className="splash-inner-div">
                 <h1 className="copytag">© 2022 by Ann Kim created with React</h1>
                 <div className="splash-top-div">
-                    <h1 className="splash-header">It's nice to meet you,</h1>
-                    <h1 className="splash-header">I'm <span> Ann Kim.</span></h1>
-                    <p className="splash-descrip">I'm a product manager, coder, & dog lover. My career, thus far, has been learning about the product.</p>
+                    <a.div className="splash-top-wrapper" style={animateDown} >
+                        <h1 className="splash-header">It's nice to meet you,</h1> 
+                        <h1 className="splash-header">I'm <span> Ann Kim.</span></h1>
+                        <p className="splash-descrip">I'm a product manager, coder, & dog lover. My career, thus far, has been learning about the product.</p>
+                    </a.div>
+                    
                 </div>
                 <div className="splash-bottom-div">
-                   { mobile ? "" : <Timeline />} 
+                   <Timeline />
                 </div>
+                
             </div>
-          
         </div>
     )
 }
--- src/components/splash/timeline.jsx
+++ src/components/splash/timeline.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 import "./timeline.css"
-import { useSpring, animated as a } from "react-spring";
+import { useSpring, animated as a, config } from "react-spring";
 import line from "../../images/line.svg"
 import code from "../../images/code.jpg"
 import advertise from "../../images/advertise.jpeg"
@@ -8,20 +8,39 @@ import market from "../../images/market.jpg"
 
 const Timeline = () => {
 
+    const [load, setLoad] = React.useState(false)
+
+    React.useEffect(() => setLoad(true))
+
+    const animateDown = useSpring({
+        opacity: load ? 1 : 0,
+        marginTop: load ? 0 : -500, 
+        config: config.slow
+    })
+
     return (
         <div className="timeline-outer-div">
             <div className="timeline-stages-wrapper">
                 <div className="timeline-stage-div">
                     <h1 className="timeline-stage-time">OCT 2020 - PRESENT</h1>
+                    <h2 className="timeline-stage-descrip">How to <span>develop</span> the product</h2>
                     <img src={line} />
-                    <div className="timeline-stage-img-div">
-                        <img className="timeline-stage-img" src={code} />
-                        <h2 className="timeline-stage-reveal">How to develop the product</h2>
-                    </div>
+                    <img className="timeline-stage-img" src={code} />
+                </div>
+                <div className="timeline-stage-div">
+                    <h1 className="timeline-stage-time">OCT 2016 - SEP 2020</h1>
+                    <h2 className="timeline-stage-descrip">How to <span>market</span> the product</h2>
+                    <img src={line} />
+                    <img className="timeline-stage-img" src={market} />
+                </div>
+                <div className="timeline-stage-div">
+                    <h1 className="timeline-stage-time">JUN 2010 - OCT 2016</h1>
+                    <h2 className="timeline-stage-descrip">How to <span>advertise</span> the product</h2>
+                    <img src={line} />
+                    <img className="timeline-stage-img" src={advertise} />
                 </div>
                 
-                <img className="timeline-stage-img" src={market} />
-                <img className="timeline-stage-img" src={advertise} />
+                
             </div>
         </div>
     )
