7c717c6457df6fdc5362f15278d3155e78283cea
annkim10 <90731268+annkim10@users.noreply.github.com>
2022-03-08 20:25:37 -0500

    Updated splash

--- src/components/splash/splash.jsx
+++ src/components/splash/splash.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react"
 import { useSpring, animated as a, config } from "react-spring";
+import { FaLinkedinIn, FaGithub } from "react-icons/fa"
 import "./splash.css"
 import Timeline from "./timeline"
 
@@ -17,20 +18,28 @@ const Splash = () => {
         config: config.slow
     })
 
-    console.log("load", load)
-
-
     return (
         <div className="splash-outer-div">
             <div className="splash-inner-div">
-                <h1 className="copytag">© 2022 by Ann Kim created with React</h1>
+                <div className="splash-footer-wrapper">
+                    <h1 className="footer-copytag">© 2022 built by Ann Kim with React</h1>
+                    <div className="footer-links-wrapper">
+                        <div className="footer-icon-wrapper">
+                            <FaLinkedinIn className="footer-icon"/>
+                        </div>
+                        <div className="footer-icon-wrapper">
+                            <FaGithub className="footer-icon"/>
+                        </div>
+                    </div>
+                    
+                </div>
+              
                 <div className="splash-top-div">
                     <a.div className="splash-top-wrapper" style={animateDown} >
                         <h1 className="splash-header">It's nice to meet you,</h1> 
                         <h1 className="splash-header">I'm <span> Ann Kim.</span></h1>
                         <p className="splash-descrip">I'm a product manager, coder, & dog lover. My career, thus far, has been learning about the product.</p>
                     </a.div>
-                    
                 </div>
                 <div className="splash-bottom-div">
                    <Timeline />
