7c7ac272ca548a19b279571a0c206fd6a87edfd7
annkim10 <90731268+annkim10@users.noreply.github.com>
2022-03-09 16:43:15 -0500

    Updated splash animations

--- src/components/splash/animations/timeline.jsx
+++ src/components/splash/animations/timeline.jsx
@@ -13,37 +13,43 @@ const Timeline = () => {
     React.useEffect(() => setLoad(true))
 
     const animateDown = useSpring({
-        opacity: load ? 1 : 0,
         marginTop: load ? 0 : -500, 
-        config: config.molasses
+        config: config.slow
     })
 
     return (
         <div className="timeline-outer-div">
             <div className="timeline-stages-wrapper">
                 <div className="timeline-stage-div">
-                    <h1 className="timeline-stage-time">OCT 2020 - PRESENT</h1>
                     <a.div className="stage-wrapper" style={animateDown}>
+                        <h1 className="timeline-stage-time">OCT 2020 - PRESENT</h1>
                         <h2 className="timeline-stage-descrip">How to <span>develop</span> the product</h2>
                         <img className="timeline-line" src={line} />
-                        <img className="timeline-stage-img" src={code} />
+                        <a.div style={animateDown}>
+                            <img className="timeline-stage-img" src={code} /> 
+                        </a.div>
                     </a.div>
-                    
                 </div>
                 <div className="timeline-stage-div">
-                    <h1 className="timeline-stage-time">OCT 2016 - SEP 2020</h1>
-                    <h2 className="timeline-stage-descrip">How to <span>market</span> the product</h2>
-                    <img className="timeline-line" src={line} />
-                    <img className="timeline-stage-img" src={market} />
+                    <a.div className="stage-wrapper" style={animateDown}>
+                        <h1 className="timeline-stage-time">OCT 2016 - SEP 2020</h1>
+                        <h2 className="timeline-stage-descrip">How to <span>market</span> the product</h2>
+                        <img className="timeline-line" src={line} />
+                        <a.div style={animateDown}>
+                            <img className="timeline-stage-img" src={market} /> 
+                        </a.div>
+                    </a.div>
                 </div>
                 <div className="timeline-stage-div">
-                    <h1 className="timeline-stage-time">JUN 2010 - OCT 2016</h1>
-                    <h2 className="timeline-stage-descrip">How to <span>advertise</span> the product</h2>
-                    <img className="timeline-line" src={line} />
-                    <img className="timeline-stage-img" src={advertise} />
+                      <a.div className="stage-wrapper" style={animateDown}>
+                        <h1 className="timeline-stage-time">JUN 2010 - OCT 2016</h1>
+                        <h2 className="timeline-stage-descrip">How to <span>advertise</span> the product</h2>
+                        <img className="timeline-line" src={line} />
+                        <a.div style={animateDown}>
+                            <img className="timeline-stage-img" src={advertise} /> 
+                        </a.div>
+                    </a.div>
                 </div>
-                
-                
             </div>
         </div>
     )
--- src/components/splash/splash.jsx
+++ src/components/splash/splash.jsx
@@ -15,10 +15,14 @@ const Splash = () => {
                     <h1 className="footer-copytag">© 2022 built by Ann Kim with React</h1>
                     <div className="footer-links-wrapper">
                         <div className="footer-icon-wrapper">
-                            <FaLinkedinIn className="footer-icon"/>
+                            <a href="https://www.linkedin.com/in/ann-kim" target="_blank">
+                                <FaLinkedinIn className="footer-icon"/>
+                            </a>
                         </div>
                         <div className="footer-icon-wrapper">
-                            <FaGithub className="footer-icon"/>
+                            <a href="https://github.com/annkim10" target="_blank">
+                                <FaGithub className="footer-icon"/>
+                            </a>
                         </div>
                     </div>
                     
