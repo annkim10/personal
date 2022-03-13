f4a3abf28d17da8685b981236bd6a4208cfc59b4
annkim10 <90731268+annkim10@users.noreply.github.com>
2022-03-12 22:20:42 -0500

    Updated about & contact mobile

--- src/components/experience/market/animations/videowrapper.jsx
+++ src/components/experience/market/animations/videowrapper.jsx
@@ -11,7 +11,7 @@ const VideoWrapper = ({visible, mobile}) => {
             mass: 1,
             tension: 280,
             friction: 180,
-            duration: 1200,
+            duration: 2000,
             easing: easings.easeOutQuad
         }
     })
